7696e94965979a289ddf1f33ef97b0c632aefdc7
Shreyash Sahare <shsa7246@colorado.edu>
2025-11-16 01:06:38 -0700

    Added requirements.txt file to deep research

--- src/deep-research/deep_research.py
+++ src/deep-research/deep_research.py
@@ -19,4 +19,5 @@ with gr.Blocks(theme=gr.themes.Default(primary_hue="sky")) as ui:
     run_button.click(fn=run, inputs=query_textbox, outputs=report)
     query_textbox.submit(fn=run, inputs=query_textbox, outputs=report)
 
-ui.launch(inbrowser=True)
+if __name__ == "__main__":
+    ui.launch()
